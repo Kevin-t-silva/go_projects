d48aa1e609282d5c9f76ca640fa15c7a78058a23
kevin.silva <kevin.t.silva@protonmail.com>
2024-06-07 09:29:10 -0300

    new test

--- teste1/hello.go
+++ teste1/hello.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 const englishHelloprefix = "Hello, "
+const chooseLanguage = "Portuguese"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == chooseLanguage {
+		return "Olá, " + name
 	}
 	return englishHelloprefix + name
 }
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
--- teste1/hello_test.go
+++ teste1/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		if got != want {
@@ -12,9 +12,16 @@ func TestHello(t *testing.T) {
 		}
 	})
 	t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+	t.Run("in Portuguese", func(t *testing.T) {
+		got := Hello("João", "Portuguese")
+		want := "Olá, João"
 		if got != want {
 			assertCorrectMessage(t, got, want)
 		}
